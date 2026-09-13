9f04f7149f2ce1c0b86f49bdec7ca0a3cc647609
franko <frankocheff99@gmail.com>
2026-09-13 00:00:49 -0600

    Update reCAPTCHA Enterprise Site Key to 6LeNx7gtAAAAAPCFE5ZnK_cU7WWgba-_4UIDe7YK

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 const app = express();
 app.use(express.json());
 
-const SITE_KEY = "6LeNx7gtAAAAAPqpo5Hcnn7-JCpOp_GML8W6T01N";
+const SITE_KEY = "6LeNx7gtAAAAAPCFE5ZnK_cU7WWgba-_4UIDe7YK";
 const GCP_PROJECT_ID = process.env.GCP_PROJECT_ID || "chef4youbyfranko";
 const API_KEY = process.env.GCP_API_KEY || "TU_API_KEY_AQUI";
 
